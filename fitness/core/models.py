1a2d98497021e0476f0a29442e80ed32c313cb2b
Ryan Castner <castner.rr@gmail.com>
2016-10-23 11:23:25 -0400

    Deleted old files

--- fitness/core/models.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-from .behaviors import Authorable, Editorable, Permalinkable, Timestampable
-from .querysets import AuthorableQuerySet
--- fitness/core/querysets.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class AuthorableQuerySet(models.query.QuerySet):
-    def authored_by(self, author):
-        return self.filter(author__username=author)
