9fdcbb68979ab9f9d28820d0ef0828343b838233
Ryan Castner <castner.rr@gmail.com>
2016-10-24 22:58:42 -0400

    Add hashids library

--- py_fitness/config/settings/common.py
+++ py_fitness/config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'attachments',
+    'hashids',
 )
 
 # Apps specific for this project go here.
