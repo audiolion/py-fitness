13bdad05a4dab28bab235569a98756e4e8fbe870
Ryan Castner <castner.rr@gmail.com>
2016-12-05 22:34:29 -0500

    Edit: redirect on login view now takes user to their workout dashboard

--- py_fitness/py_fitness/users/views.py
+++ py_fitness/py_fitness/users/views.py
@@ -32,8 +32,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse('users:detail',
-                       kwargs={'username': self.request.user.username})
+        return reverse('workout:dashboard')
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
