83cd5388d4f4008f964fb970c7b31badb849d0ac
Ryan Castner <castner.rr@gmail.com>
2017-01-01 20:52:32 -0500

    Update duration tag to handle multiple days cases

--- py_fitness/py_fitness/core/templatetags/duration.py
+++ py_fitness/py_fitness/core/templatetags/duration.py
@@ -7,11 +7,13 @@ def duration(duration):
     seconds = int(duration.total_seconds())
     hours = seconds // 3600
     minutes = (seconds % 3600) // 60
+    seconds = seconds // 3600 // 60
+    days = None
     if hours > 24:
         days = (seconds % 3600) // 24
         hours = (seconds - (days * 3600 * 24)) // 60
 
-    if days:
+    if days is None:
         return '{} hours {} minutes and {} seconds'.format(hours, minutes, seconds)
     else:
         return '{} days, {} hours {} minutes and {} seconds'.format(days, hours, minutes, seconds)
